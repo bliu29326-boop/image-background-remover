652bfab40c1849f210f48dda39ee0dfdbbdc4435
root <root@localhost.localdomain>
2026-04-05 21:26:17 +0800

    Fix OpenNext page runtime after rollback

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { auth } from "@/auth";
 import HomeClient from "./components/HomeClient";
 
-export const runtime = "edge";
-
 export default async function Home() {
   const session = await auth();
   return <HomeClient userEmail={session?.user?.email ?? null} />;
